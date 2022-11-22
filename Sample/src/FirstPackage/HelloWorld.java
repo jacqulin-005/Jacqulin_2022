95dfa03e7d62dd3f3491679514d7c103ac80bdc1
jacqulin-005 <jacqulin.005@gmail.com>
2022-11-21 23:53:54 -0500

    Created a Hello World Program

---
+++ Sample/src/FirstPackage/HelloWorld.java
@@ -0,0 +1,10 @@
+package FirstPackage;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World! This is my first program");
+
+	}
+
+}
---
+++ Sample/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Jacqulin
+ *
+ */
+module Test {
+}
